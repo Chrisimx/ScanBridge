16dddd696176a2815f44120aaf3728d6b34858f9
Christian Nagel <chris.imx@online.de>
2024-12-23 00:31:33 +0100

    Add PaperFormat type and default paper formats
    Default paper formats are A0 to A10, generated to ISO 216. Other formats will likely be added in the future

---
+++ app/src/main/java/io/github/chrisimx/scanbridge/PaperFormat.kt
@@ -0,0 +1,49 @@
+package io.github.chrisimx.scanbridge
+
+import io.github.chrisimx.esclkt.LengthUnit
+import io.github.chrisimx.esclkt.Millimeters
+import io.github.chrisimx.esclkt.millimeters
+
+// Paper format class
+data class PaperFormat(
+    val name: String,
+    val width: LengthUnit,
+    val height: LengthUnit
+)
+
+fun loadDefaultFormats(): List<PaperFormat> {
+    val defaultPaperFormats: MutableList<PaperFormat> = mutableListOf()
+
+    // calculate the width and height of the ISO 216 paper formats based on the A0 format
+    val a0 = PaperFormat("A0", width = 841.0.millimeters(), height = 1189.0.millimeters())
+    defaultPaperFormats.add(a0)
+
+    // Generate A1 to A10 formats
+    var currentWidth = a0.width.toMillimeters()
+    var currentHeight = a0.height.toMillimeters()
+
+    for (i in 1..10) {
+        // Swap dimensions (width becomes half of height)
+        val newWidth = Millimeters(currentHeight.value / 2)
+        val newHeight = currentWidth
+
+        // Swap dimensions (width becomes height, height becomes width)
+        val finalWidth = newWidth
+        val finalHeight = newHeight
+
+        // Add the new format to the list
+        defaultPaperFormats.add(
+            PaperFormat(
+                name = "A$i",
+                width = finalWidth.value.toInt().millimeters(),
+                height = finalHeight.value.toInt().millimeters()
+            )
+        )
+
+        // Update dimensions for the next iteration
+        currentWidth = finalWidth
+        currentHeight = finalHeight
+    }
+
+    return defaultPaperFormats
+}
---
+++ app/src/test/java/org/github/chrisimx/scanbridge/PaperFormatTest.kt
@@ -0,0 +1,57 @@
+/*
+ *     Copyright (C) 2024 Christian Nagel and contributors
+ *
+ *     This file is part of ScanBridge.
+ *
+ *     ScanBridge is free software: you can redistribute it and/or modify it under the terms of
+ *     the GNU General Public License as published by the Free Software Foundation, either
+ *     version 3 of the License, or (at your option) any later version.
+ *
+ *     ScanBridge is distributed in the hope that it will be useful, but WITHOUT ANY
+ *     WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *     FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along with eSCLKt.
+ *     If not, see <https://www.gnu.org/licenses/>.
+ *
+ *     SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package org.github.chrisimx.scanbridge
+
+import io.github.chrisimx.scanbridge.loadDefaultFormats
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class PaperFormatTest {
+
+    @Test
+    fun loadDefault() {
+        val expectedSizes = listOf(
+            Pair(841, 1189),  // A0
+            Pair(594, 841),   // A1
+            Pair(420, 594),   // A2
+            Pair(297, 420),   // A3
+            Pair(210, 297),   // A4
+            Pair(148, 210),   // A5
+            Pair(105, 148),   // A6
+            Pair(74, 105),    // A7
+            Pair(52, 74),     // A8
+            Pair(37, 52),     // A9
+            Pair(26, 37)      // A10
+        )
+
+        val paperFormats = loadDefaultFormats()
+
+        for (i in paperFormats.indices) {
+            val expected = expectedSizes[i]
+            val actual = paperFormats[i]
+
+            assertEquals(expected.first.toDouble(), actual.width.toMillimeters().value)
+            assertEquals(expected.second.toDouble(), actual.height.toMillimeters().value)
+        }
+
+        println(paperFormats)
+        assert(paperFormats.size == 11)
+    }
+}
