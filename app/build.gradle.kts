f7cdf65e355e0cbb2d9a1ce6e86254dfc4be02fa
Christian Nagel <chris.imx@online.de>
2025-01-15 21:30:39 +0100

    Bump version: v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "io.github.chrisimx.scanbridge"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1_000_000 // format is MAJ_MIN_PAT with always 3 digits
-        versionName = "1.0.0"
+        versionCode = 1_000_001 // format is MAJ_MIN_PAT with always 3 digits
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
