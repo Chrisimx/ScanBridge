c8972c3a3661fc920213f970b170fc06d051de76
Christian Nagel <chris.imx@online.de>
2026-02-18 19:19:33 +0100

    Remove unnecessary test code replacements

---
+++ app/src/main/java/io/github/chrisimx/scanbridge/stores/SessionsStore.kt
@@ -0,0 +1,62 @@
+package io.github.chrisimx.scanbridge.stores
+
+import android.content.Context
+import androidx.lifecycle.application
+import io.github.chrisimx.esclkt.Inches
+import io.github.chrisimx.esclkt.LengthUnit
+import io.github.chrisimx.esclkt.Millimeters
+import io.github.chrisimx.esclkt.Points
+import io.github.chrisimx.esclkt.ThreeHundredthsOfInch
+import io.github.chrisimx.scanbridge.data.model.Session
+import io.github.chrisimx.scanbridge.data.model.Session.Companion.fromString
+import java.io.File
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToStream
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
+import timber.log.Timber
+
+object SessionsStore {
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+        serializersModule = SerializersModule {
+            polymorphic(LengthUnit::class) {
+                subclass(Inches::class)
+                subclass(Millimeters::class)
+                subclass(ThreeHundredthsOfInch::class)
+                subclass(Points::class)
+            }
+        }
+        classDiscriminator = "type"
+        prettyPrint = false
+    }
+
+    fun loadSession(application: Context, sessionID: String): Session? {
+        val path = application.applicationInfo.dataDir + "/files/" + sessionID + ".session"
+        val file = File(path)
+
+        if (!file.exists()) {
+            Timber.d("Could not find session file at $path")
+            return null
+        }
+
+        val sessionFileString = file.readText()
+
+        return fromString(sessionFileString, json)
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    fun saveSession(session: Session, application: Context, sessionID: String): String {
+        val path = application.applicationInfo.dataDir + "/files/" + sessionID + ".session"
+        val file = File(path)
+
+        file.outputStream().use {
+            json.encodeToStream(session, it)
+        }
+
+        return path
+    }
+}
