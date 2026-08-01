644b34aa109b9e6a1b9b9903790a45d5478b5024
Christian Nagel <chris.imx@online.de>
2026-08-01 18:29:23 +0200

    Fix play flavor of ScanBridge

--- androidApp/src/play/java/io/github/chrisimx/scanbridge/StartupTabDefinitions.kt
+++ androidApp/src/play/java/io/github/chrisimx/scanbridge/StartupTabDefinitions.kt
@@ -10,29 +10,27 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 @OptIn(ExperimentalMaterial3Api::class)
 val STARTUP_TABS = listOf(
     StartupScreen(
-        io.github.chrisimx.scanbridge.R.string.discovery,
-        io.github.chrisimx.scanbridge.R.string.header_scannerbrowser,
+        R.string.discovery,
+        R.string.header_scannerbrowser,
         Icons.Filled.Home,
         Icons.Outlined.Home,
         true,
-        { innerPadding, navController, showCustomDialog, setShowCustomDialog, statefulScannerMap, statefulScannerMapSecure ->
+        { innerPadding, navController, showCustomDialog, setShowCustomDialog ->
             ScannerBrowser(
                 innerPadding,
                 navController,
                 showCustomDialog,
                 setShowCustomDialog,
-                statefulScannerMap,
-                statefulScannerMapSecure
             )
         }
     ),
     StartupScreen(
-        io.github.chrisimx.scanbridge.R.string.settings,
-        io.github.chrisimx.scanbridge.R.string.settings,
+        R.string.settings,
+        R.string.settings,
         Icons.Filled.Settings,
         Icons.Outlined.Settings,
         false,
-        { innerPadding, _, _, _, _, _ ->
+        { innerPadding, _, _, _ ->
             AppSettingsScreen(innerPadding)
         }
     ),
@@ -42,7 +40,7 @@ val STARTUP_TABS = listOf(
         BaselineHelp24,
         OutlineHelp24,
         false,
-        { innerPadding, _, _, _, _, _ ->
+        { innerPadding, _, _, _ ->
             SupportScreen(innerPadding)
         }
     )
